ce17a7b8b375ba62e2c58e6a4be0203e14a1b77a
TannerOliver <435.tanner@gmail.com>
2021-11-16 17:52:20 -0600

    populating webpage

--- README.md
+++ README.md
@@ -1,8 +1,43 @@
-## Hw06-weather-dashboard
-# Objective
-In this homework we are suppose to use the API to pull the data and display it in the respective spots in the webpage. Then we are suppose to store the previously searched towns in a list below the searched list which I created a whole seperate area for. I had a hard time using bootstrap so I just did standard CSS and some flex to try and get it looking alright.
-# Screenshot
-![weatherScreenshot1](./assets/images/weather1.png)
-![weatherScreenshot2](./assets/images/weather2.png)
-# Deployed Page
-https://tanneroliver.github.io/hw06-weather-dashboard/
+# hw06-Weather Dashboard
+
+![license](https://img.shields.io/badge/license-MIT-red)
+
+## Description
+
+In this application you should be able to search for a cities weather. You should be returned with the current weather and the forecast for the week.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contribution](#contribution)
+- [Test](#test)
+- [Questions](#questions)
+
+## Installation
+
+You should just be able to use it in the browser. Just clone th repo.
+
+## Usage
+
+Search for a city using the search bar. cities you have searched for in the past should show up under that bar.
+![weather_dashboard1](./assets/images/weather_dashboard1.png)
+
+## License
+
+This project is under the MIT license.
+
+## Contribution
+
+bootstrap, jquery
+
+## Test
+
+Look for weather in different cities.
+
+## Questions
+
+My GitHub is: [TannerOliver](https://github.com/TannerOliver)  
+You can reach me at Email: 435.tanner@gmail.com  
+Check out the deployed page! [GitHub](https://tanneroliver.github.io/hw06-weather-dashboard/)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,109 +1,143 @@
+let searchBtn = $(".searchBtn");
+let cityName = $("input.city");
+let savedCitiesDiv = $("#savedCities")
+let today = new Date();
+let storedCities = JSON.parse(localStorage.getItem('cities')) || []
 
-// GIVEN a weather dashboard with form inputs
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-// WHEN I view the UV index
-// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
+let tomorrow = new Date();
+tomorrow.setDate(today.getDate()+1)
 
-//  Selectors
-var searchBtn = $(".searchBtn");
+let tomorrow2 = new Date();
+tomorrow2.setDate(tomorrow.getDate()+1)
 
-//  Variables
-var cityName = "Minneapolis"; //$(".text").val("Minneapolis");
-var lat;
-var lon;
-var requestUrl;
-var currentData;
+let tomorrow3 = new Date();
+tomorrow3.setDate(tomorrow2.getDate()+1)
 
-//  Functions
-searchBtn.on("click", function () {
-  requestUrl =
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-    cityName +
-    "&appid=81c8f33d2835754b525076279bdd2d53";
-  getApi();
-});
+let tomorrow4 = new Date();
+tomorrow4.setDate(tomorrow3.getDate()+1)
+let tomorrow5 = new Date();
+tomorrow5.setDate(tomorrow4.getDate()+1)
 
-function getApi(currentData) {
-  //getting the first url with lat and lon vars
+function getApi(requestUrl) {
+  console.log(cityName)
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       console.log(data);
-      lat = data.coord.lat;
-      lon = data.coord.lon;
-      //this is where I start fetching the second url with the weather data
-      var requestUrl2 =
-        "https://api.openweathermap.org/data/2.5/onecall?lat=" +
-        lat +
-        "&lon=" +
-        lon +
-        "&appid=81c8f33d2835754b525076279bdd2d53";
+      let lat = data.coord.lat;
+      let lon = data.coord.lon;
+      console.log(lat);
+      let requestUrl2 =
+        "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=81c8f33d2835754b525076279bdd2d53";
       fetch(requestUrl2)
         .then(function (response) {
           return response.json();
         })
         .then(function (data) {
-          currentData = data;
-          return currentData
+          console.log(data);
+          appendingCurrentHtml(data);
+          appendingForecast(data);
         });
     });
 }
 
-//  Calling Funtions
+function appendingCurrentHtml(data) {
+  let currentCity = document.getElementById('curCity');
+  let currentTemp = document.getElementById('curTemp');
+  let currentWind = document.getElementById('curWind');
+  let currentHumid = document.getElementById('curHumid');
+  let currentUvIndex = document.getElementById('curUVI');
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//  ACCEPTANCE CRITERIA
-//    GIVEN a weather dashboard with form inputs
-//      WHEN I search for a city
-//        THEN I am presented with current and future conditions for that city and that city is added to the search history
-//      WHEN I view current weather conditions for that city
-//        THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-//      WHEN I view the UV index
-//        THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-//      WHEN I view future weather conditions for that city
-//        THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-//      WHEN I click on a city in the search history
-//        THEN I am again presented with current and future conditions for that city
+  currentCity.textContent = cityName.val() + " " + today;
+  currentTemp.textContent = 'Temp: ' + data.current.temp + '°F';
+  currentWind.textContent = 'Wind: ' + data.current.wind_speed + ' MPH';
+  currentHumid.textContent = 'Humidtiy: ' + data.current.humidity + ' %';
+  currentUvIndex.textContent = 'UV Index: ' + data.current.uvi;
+};
 
-//  Possible Selectors
+function appendingForecast(data) {
+  let forecast1 = document.getElementById('1');
+  let forecast2 = document.getElementById('2');
+  let forecast3 = document.getElementById('3');
+  let forecast4 = document.getElementById('4');
+  let forecast5 = document.getElementById('5');
 
-// select search field
-//select search btn
-//select field to input prevvious city savedCities
-//select current-city-container
-//select 5-day forecast cards
+  let template = `<br>
+  <p>${tomorrow}</p>
+  <p>emoji</p>
+  <p>Temp: ${data.daily[1].temp.day}°F</p>
+  <p>Wind: ${data.daily[1].wind_speed} MPH</p>
+  <p>Humidity: ${data.daily[1].humidity} %</p>`;
+  forecast1.insertAdjacentHTML('beforeend', template);
 
-//  Possible variables I will need
+  let template2 = `<br>
+  <p>${tomorrow2}</p>
+  <p>emoji</p>
+  <p>Temp: ${data.daily[2].temp.day}°F</p>
+  <p>Wind: ${data.daily[2].wind_speed} MPH</p>
+  <p>Humidity: ${data.daily[2].humidity} %</p>`;
+  forecast2.insertAdjacentHTML('beforeend', template2);
 
-//use moment.js to get date for app
-//define single source of truth for previous cities going to local storage.
-//  define variables for current weather data where I can store api call data there will be a lot of them.
+  let template3 = `<br>
+  <p>${tomorrow3}</p>
+  <p>emoji</p>
+  <p>Temp: ${data.daily[3].temp.day}°F</p>
+  <p>Wind: ${data.daily[3].wind_speed} MPH</p>
+  <p>Humidity: ${data.daily[3].humidity} %</p>`;
+  forecast3.insertAdjacentHTML('beforeend', template3);
 
+  let template4 = `<br>
+  <p>${tomorrow4}</p>
+  <p>emoji</p>
+  <p>Temp: ${data.daily[4].temp.day}°F</p>
+  <p>Wind: ${data.daily[4].wind_speed} MPH</p>
+  <p>Humidity: ${data.daily[4].humidity} %</p>`;
+  forecast4.insertAdjacentHTML('beforeend', template4);
 
-//  Possible Functions and or functionalities
+  let template5 = `<br>
+  <p>${tomorrow5}</p>
+  <p>emoji</p>
+  <p>Temp: ${data.daily[5].temp.day}°F</p>
+  <p>Wind: ${data.daily[5].wind_speed} MPH</p>
+  <p>Humidity: ${data.daily[5].humidity} %</p>`;
+  forecast5.insertAdjacentHTML('beforeend', template5);
+};
 
-// fetch data from weather
-//fetch data from one call
-//function to get local storage or set local storage or maybe seperate functions for both
-//function that calls the first api takes lat and lon and goes to another function that calls second api and builds any html from data it returns
-//function for calling second api that builds anything that api data returns then returns response to first api
-//posibly have a function that builds all html in current day
-//possibly have function that builds 5 day forecast
+function populateCity() {
+  console.log(savedCitiesDiv);
+  for (let i = 0; i < storedCities.length; i++) {
+    console.log(storedCities[i]);
+    let setTemp = `<button>${storedCities[i]}</button>`;
+    savedCitiesDiv.append(setTemp);
+  }
+};
 
-//  What I can get from some of the API's
+function setCity() {
+  if (!storedCities.includes(cityName.val())){
+    storedCities.push(cityName.val());
+    localStorage.setItem('cities', JSON.stringify(storedCities));
+  }
+};
 
-//Current weather data I can call and get current weather data  and lat/lon that one call will need has an icon I can use to make icon on my page
-//one call API should return current weather and a 7 day forecast which I can use to build my cards I will need lat and lon for that this will also have uv index I think
+savedCitiesDiv.on('click', (event) => {
+  const isButton = event.target.nodeName === 'BUTTON';
+  if (!isButton) {
+    return;
+  }
+  console.dir(event.target.innerHTML);
+  let requestUrl =
+    "https://api.openweathermap.org/data/2.5/weather?q=" + event.target.innerHTML + "&appid=81c8f33d2835754b525076279bdd2d53";
+    getApi(requestUrl);
+})
 
-//  Event Listener and what they should do
+searchBtn.on("click", function () {
+  console.log(cityName.val())
+  let requestUrl =
+    "https://api.openweathermap.org/data/2.5/weather?q=" + cityName.val() + "&appid=81c8f33d2835754b525076279bdd2d53";
+  getApi(requestUrl);
+  setCity();
+});
 
-// on button click search weather api to get lat and lon for one call api
+populateCity();
